8131f3be840addf3fc8b77581b82296d6fa8b3b4
maksimbalantsevich <easypeasylemonskvii@gmail.com>
2023-01-20 14:57:24 +0300

    add benefits block

--- client/src/components/layouts/Main.tsx
+++ client/src/components/layouts/Main.tsx
@@ -11,10 +11,10 @@ const MainLayout = () => {
         <img className='main_wrapper-img' src="https://i.redd.it/c3uhsgo1vx541.jpg" alt="a" />
         <img className='main_wrapper-img' src='https://c4.wallpaperflare.com/wallpaper/39/346/426/digital-art-men-city-futuristic-night-hd-wallpaper-thumb.jpg' alt='a'/>
       </ImageSlider> */}
-      <section className='main_wrapper'>
+      <section className='main_wrapper mg-btm-70'>
         <img className='main_wrapper-img' src="https://c4.wallpaperflare.com/wallpaper/39/346/426/digital-art-men-city-futuristic-night-hd-wallpaper-thumb.jpg" alt="a" />
       </section>
-      <Container>
+      <Container className={'mg-btm-70'}>
         <ImageSlider className={'main_slider'}>
           <TransportationType/>
           <TransportationType/>
@@ -24,9 +24,21 @@ const MainLayout = () => {
         </ImageSlider>
       </Container>
 
-      {/* <section className='main_wrapper'>
-        <img className='main_wrapper-img' src="image.png" alt="a" />
-      </section> */}
+      <section className='main_wrapper'>
+        <img className='main_wrapper-img' src="https://www.iveco.com/netherlands/PublishingImages/Jost%20Group_IVECO%20fleet%20(1600x1115)%20-%20kopie.jpg" alt="a" />
+      </section>
+      <h1 className='main_title'>Преимущества</h1>
+
+      <Container className={'mg-btm-70'}>
+        <ImageSlider className={'main_slider'}>
+          <TransportationType/>
+          <TransportationType/>
+          <TransportationType/>
+          <TransportationType/>
+          <TransportationType/>
+        </ImageSlider>
+      </Container>
+
     </main>
   )
 }
