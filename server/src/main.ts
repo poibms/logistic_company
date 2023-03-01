bd768c3f56ec319bceb0af0e6e8d162f6c73daae
maksimbalantsevich <easypeasylemonskvii@gmail.com>
2023-03-01 13:19:41 +0300

    add: coockie parser

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const port = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(port, () => console.log(`started on ${port}`));
 }
