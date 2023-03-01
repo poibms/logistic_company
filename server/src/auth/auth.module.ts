d92c6c70cb6f62d17ba66cddc14587d6ce52e934
maksimbalantsevich <easypeasylemonskvii@gmail.com>
2023-03-01 15:17:01 +0300

    add: refresh token logic

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { JwtStrategy } from './jwt.strategy';
+import { JwtStrategy } from './strategy/jwt.strategy';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
@@ -7,19 +7,15 @@ import { User } from './user.entity';
 import { UserRepository } from './user.repository';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { RefreshTokenStrategy } from './strategy/jwt-refresh.strategy';
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
-    JwtModule.register({
-      secret: 'topsecret1',
-      signOptions: {
-        expiresIn: 3600,
-      },
-    }),
+    JwtModule.register({}),
     TypeOrmModule.forFeature([User]),
   ],
-  providers: [AuthService, UserRepository, JwtStrategy],
+  providers: [AuthService, UserRepository, JwtStrategy, RefreshTokenStrategy],
   controllers: [AuthController],
   exports: [JwtStrategy, PassportModule],
 })
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,59 +1,104 @@
-import { AuthCredsDto } from './dto/auth-creds.dto';
 import { AuthSignInDto } from './dto/auth-signin.dto';
 import { AuthSignUpDto } from './dto/auth-signup.dto';
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  UnauthorizedException,
+  BadRequestException,
+  ForbiddenException,
+} from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
-import { JwtPayload } from './jwt-payload.interface';
 import { User } from './user.entity';
+import { ConfigService } from '@nestjs/config';
+import { TokensType } from 'src/types/token.types';
+import { JwtPayload } from 'dist/auth/jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
   constructor(
     private userRepository: UserRepository,
     private jwtService: JwtService,
+    private config: ConfigService,
   ) {}
 
-  async signUp(authSignUpDto: AuthSignUpDto): Promise<AuthCredsDto> {
-    const user = await this.userRepository.signUp(authSignUpDto);
-    const token = await this.genAccesToken(user);
-    return {
-      user: {
-        id: user.id,
-        email: user.email,
-        name: user.name,
-        surname: user.surname,
-        role: user.role,
-      },
-      accessToken: token,
-    };
+  async signUp(authSignUpDto: AuthSignUpDto): Promise<TokensType> {
+    const { password } = authSignUpDto;
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await this.userRepository.signUp({
+      ...authSignUpDto,
+      password: hashedPassword,
+    });
+    const tokens = await this.genAccesToken(user);
+    await this.userRepository.updateUserRefreshToken(
+      user.id,
+      tokens.refresh_token,
+    );
+    return tokens;
   }
 
-  async signIn(authSignInDto: AuthSignInDto): Promise<AuthCredsDto> {
+  async signIn(authSignInDto: AuthSignInDto): Promise<TokensType> {
     const { email, password } = authSignInDto;
 
     const user = await this.userRepository.getUserByEmail(email);
     if (user && (await bcrypt.compare(password, user.password))) {
-      const token = await this.genAccesToken(user);
-      return {
-        user: {
-          id: user.id,
-          email: user.email,
-          name: user.name,
-          surname: user.surname,
-          role: user.role,
-        },
-        accessToken: token,
-      };
+      const tokens = await this.genAccesToken(user);
+      await this.userRepository.updateUserRefreshToken(
+        user.id,
+        tokens.refresh_token,
+      );
+
+      return tokens;
     } else {
       throw new UnauthorizedException('Please check your credentials');
     }
   }
 
-  private async genAccesToken(user: User): Promise<string> {
-    const payload: JwtPayload = { id: user.id, email: user.email };
-    const accessToken: string = await this.jwtService.sign(payload);
-    return accessToken;
+  async logout(userId: string): Promise<boolean> {
+    return await this.userRepository.logout(userId);
+  }
+
+  async refreshToken(
+    userId: string,
+    refreshToken: string,
+  ): Promise<TokensType> {
+    const user = await this.userRepository.getUserById(userId);
+    if (!user || !user.rthash) throw new BadRequestException('Access denied');
+
+    const isRefreshMatch = await bcrypt.compare(refreshToken, user.rthash);
+    if (!isRefreshMatch) throw new ForbiddenException('Access Denied');
+
+    const tokens = await this.genAccesToken(user);
+    await this.userRepository.updateUserRefreshToken(
+      user.id,
+      tokens.refresh_token,
+    );
+
+    return tokens;
+  }
+
+  private async genAccesToken(user: User): Promise<TokensType> {
+    const jwtPayload: JwtPayload = {
+      id: user.id,
+      email: user.email,
+    };
+
+    const [accesToken, refreshToken] = await Promise.all([
+      this.jwtService.signAsync(jwtPayload, {
+        secret: this.config.get<string>('JWT_ACCESS_SECRET'),
+        expiresIn: '15m',
+      }),
+      this.jwtService.signAsync(jwtPayload, {
+        secret: this.config.get<string>('JWT_REFRESH_SECRET'),
+        expiresIn: '7d',
+      }),
+    ]);
+
+    return {
+      access_token: accesToken,
+      refresh_token: refreshToken,
+    };
   }
 }
