deb1bcc0e9cf1ab6144d53b55700160433849795
maksimbalantsevich <easypeasylemonskvii@gmail.com>
2023-03-07 15:48:20 +0300

    add: whitelist params to validation pipes

--- server/src/main.ts
+++ server/src/main.ts
@@ -9,7 +9,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(cookieParser());
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   app.setGlobalPrefix('/api');
 
   const config = new DocumentBuilder()
