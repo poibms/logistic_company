cb7933b17ebbf8756a08103e602087dd51e373ce
maksimbalantsevich <easypeasylemonskvii@gmail.com>
2023-03-12 23:33:30 +0300

    add: adminPanel nav ui

--- client/src/components/common/AdminPanelNav/AdminPanelNav.tsx
+++ client/src/components/common/AdminPanelNav/AdminPanelNav.tsx
@@ -1,7 +1,53 @@
 import React from "react";
+import DashboardIcon from "@mui/icons-material/Dashboard";
 
 const AdminPanelNav = () => {
-  return <></>;
+  return (
+    <div className="navpanel">
+      <div className="navpanel_wrapper">
+        <div className="navpanel_img">
+          <img
+            src="https://cdn.logo.com/hotlink-ok/logo-social.png"
+            alt="logo"
+          />
+        </div>
+        <div className="navpanel_menu">
+          <ul className="navpanel_items">
+            <li>
+              <DashboardIcon />
+              <span>Dashboard</span>
+            </li>
+            <li>
+              <DashboardIcon />
+              <span>Dashboard</span>
+            </li>
+            <li>
+              <DashboardIcon />
+              <span>Dashboard</span>
+            </li>
+            <li>
+              <DashboardIcon />
+              <span>Dashboard</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="navpanel_submenu">
+        <div className="navpanel_wrapper">
+          <ul className="navpanel_items">
+            <li>
+              <DashboardIcon />
+              <span>Dashboard</span>
+            </li>
+            <li>
+              <DashboardIcon />
+              <span>Dashboard</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AdminPanelNav;
--- client/src/components/pages/AdminPage.tsx
+++ client/src/components/pages/AdminPage.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import AdminPanelNav from '../common/AdminPanelNav/AdminPanelNav';
 
 const AdminPage: React.FC = () => {
   return (
     <div className='admin_wrapper'>
+      <AdminPanelNav/>
     </div>
   )
 }
