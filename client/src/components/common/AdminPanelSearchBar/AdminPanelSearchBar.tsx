a4750ccfe8f5ebfcd4e1f5c7f1ac24dda8a2f2ab
maksimbalantsevich <easypeasylemonskvii@gmail.com>
2023-03-13 13:55:21 +0300

    add: admin search bar ui

--- client/src/components/common/AdminPanelSearchBar/AdminPanelSearchBar.tsx
+++ client/src/components/common/AdminPanelSearchBar/AdminPanelSearchBar.tsx
@@ -1,7 +1,23 @@
 import * as React from "react";
+import InputField from "../InputField/InputField";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const AdminPanelSearchBar = () => {
-  return <></>;
+  return (
+    <div className="searchbar">
+      <div className="searchbar_wrapper">
+        <div className="searchbar_title">
+          <h3>DASHBOARD</h3>
+        </div>
+        <div className="searchbar_input">
+          <InputField name='search' placeholder='Search orders, drivers, trucks' />
+        </div>
+        <div>
+          <AccountCircleIcon />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AdminPanelSearchBar;
--- client/src/components/pages/AdminPage.tsx
+++ client/src/components/pages/AdminPage.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import AdminPanelNav from '../common/AdminPanelNav/AdminPanelNav';
+import React from "react";
+import AdminPanelNav from "../common/AdminPanelNav/AdminPanelNav";
+import AdminPanelSearchBar from "../common/AdminPanelSearchBar/AdminPanelSearchBar";
 
 const AdminPage: React.FC = () => {
   return (
-    <div className='admin_wrapper'>
-      <AdminPanelNav/>
+    <div className="admin_wrapper">
+      <AdminPanelNav />
+      <div className="admin_content-wrapper">
+        <AdminPanelSearchBar />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default AdminPage;
