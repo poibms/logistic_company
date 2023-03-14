8338558bf8e0a296f1c1f04b3132013e935c4ea0
maksimbalantsevich <easypeasylemonskvii@gmail.com>
2023-03-14 11:32:58 +0300

    refactor admin page

--- client/src/components/pages/AdminPage.tsx
+++ client/src/components/pages/AdminPage.tsx
@@ -1,13 +1,21 @@
 import React from "react";
+import AdminList from "../common/AdminList/AdminList";
 import AdminPanelNav from "../common/AdminPanelNav/AdminPanelNav";
 import AdminPanelSearchBar from "../common/AdminPanelSearchBar/AdminPanelSearchBar";
+import { useSelector } from 'react-redux';
+import { getAllOrders } from "../../store/orders";
 
 const AdminPage: React.FC = () => {
+  const orders = useSelector(getAllOrders());
+
   return (
     <div className="admin_wrapper">
       <AdminPanelNav />
-      <div className="admin_content-wrapper">
+      <div className="admin_content">
         <AdminPanelSearchBar />
+        <div className="flex admin_content-wrapper">
+          <AdminList data={orders}/>
+        </div>
       </div>
     </div>
   );
