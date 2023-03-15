aaa86af38012188416c6dab3298f1f0006717d48
maksimbalantsevich <easypeasylemonskvii@gmail.com>
2023-03-15 12:45:32 +0300

    add: map ui

--- client/src/components/common/Map/Map.tsx
+++ client/src/components/common/Map/Map.tsx
@@ -1,10 +1,28 @@
-import * as React from 'react';
+import { YMaps, Map as YandexMap, Placemark } from "@pbe/react-yandex-maps";
+import * as React from "react";
 
 const Map = () => {
+  console.log('loading')
   return (
     <>
+      <div className="map_wrapper">
+        <YMaps>
+          <YandexMap
+            defaultState={{
+              center: [55.75, 37.57],
+              zoom: 9,
+              controls: ["zoomControl", "fullscreenControl"],
+            }}
+            width='100%'
+            height='100%'
+            modules={["control.ZoomControl", "control.FullscreenControl"]}
+          >
+            <Placemark defaultGeometry={[55.751574, 37.573856]} />
+          </YandexMap>
+        </YMaps>
+      </div>
     </>
   );
-}
- 
+};
+
 export default Map;
