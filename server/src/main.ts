beb93adaf78c03ea647242570a95b569feb7e4eb
maksimbalantsevich <easypeasylemonskvii@gmail.com>
2023-03-15 19:04:40 +0300

    add: cors options

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,7 +7,12 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const port = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+  };
+  app.enableCors(corsOptions);
   app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
