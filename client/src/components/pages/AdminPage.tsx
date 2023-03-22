75c1161f598ec0c2928a6f986d4ce702c1849f77
maksimbalantsevich <easypeasylemonskvii@gmail.com>
2023-03-22 18:27:24 +0300

    add: load modal on admin page

--- client/src/components/pages/AdminPage.tsx
+++ client/src/components/pages/AdminPage.tsx
@@ -11,19 +11,28 @@ import { getAllTrucks } from "../../store/trucks";
 import DriverDeteledInfo from "../common/DeteledItemInfo/DriverDeteledInfo/DriverDeteledInfo";
 import TruckDeteledInfo from "../common/DeteledItemInfo/TruckdeteledInfo/TruckDeteledInfo";
 import AdminFilter from "../common/AdminFilter/AdminFilter";
+import BasicModal from "../ui/Modal/Modal";
+import AddDriverForm from "../ui/AddDriverForm/AddDriverForm";
+import AddTuckForm from "../ui/AddTruckForm/AddTruckForm";
 
 const AdminPage: React.FC = memo(() => {
   const [load, setLoad] = useState(false);
   const [data, setData] = useState({} as OrderType | DriverType | TruckType);
   const [filterIndex, setFilterIndex] = useState(0);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false);
+  };
   const [dataType, setDataType] = useState("orders");
   const orders = useSelector(getAllOrders());
   const drivers = useSelector(getAllDrivers());
   const trucks = useSelector(getAllTrucks());
 
-
-  const handleChangeFilterIndex = (event: React.SyntheticEvent, newValue: any) => {
-    console.log(newValue)
+  const handleChangeFilterIndex = (
+    event: React.SyntheticEvent,
+    newValue: any
+  ) => {
     setFilterIndex(newValue);
   };
 
@@ -34,56 +43,63 @@ const AdminPage: React.FC = memo(() => {
   const changeDataType = (dataType: string) => {
     setDataType(dataType);
     setLoad(false);
+    setFilterIndex(0);
   };
 
   const genOrdersEntityByFilter = () => {
     switch (filterIndex) {
       case 0:
-        return orders.filter((order) => order.status === 'not_assigned')
-      case 1: 
-        return orders.filter((order) => order.status === 'in_progress')
-      case 2: 
-        return orders.filter((order) => order.status === 'done')
-      default: 
-        return orders.filter((order) => order.status === 'not_assigned')
+        return orders.filter((order) => order.status === "not_assigned");
+      case 1:
+        return orders.filter((order) => order.status === "in_progress");
+      case 2:
+        return orders.filter((order) => order.status === "done");
+      default:
+        return orders.filter((order) => order.status === "not_assigned");
     }
-  }
+  };
 
   const genDriversEntityByFilter = () => {
     switch (filterIndex) {
       case 0:
-        return drivers.filter((order) => order.truckId === null)
-      case 1: 
-        return drivers.filter((order) => order.truckId !== null)
-      default: 
-        return drivers.filter((order) => order.truckId === null)
+        return drivers.filter((order) => order.truckId === null);
+      case 1:
+        return drivers.filter((order) => order.truckId !== null);
+      default:
+        return drivers.filter((order) => order.truckId === null);
     }
-  }
+  };
 
   const samplingDataEntity = () => {
     if (dataType === "orders") {
       const map = <Map data={data as OrderType} />;
-      const filteredOrders = genOrdersEntityByFilter()
-      return { entity: filteredOrders, deteledInfo: map };
+      const filteredOrders = genOrdersEntityByFilter();
+      return { entity: filteredOrders, deteledInfo: map, modal: null };
     } else if (dataType === "drivers") {
       const info = <DriverDeteledInfo driver={data as DriverType} />;
       const filteredDrivers = genDriversEntityByFilter();
-      return { entity: filteredDrivers, deteledInfo: info };
+      const driverModal = <AddDriverForm />;
+      return { entity: filteredDrivers, deteledInfo: info, modal: driverModal };
     } else {
       const info = <TruckDeteledInfo truck={data as TruckType} />;
-      return { entity: trucks, deteledInfo: info };
+      const truckModal = <AddTuckForm />;
+      return { entity: trucks, deteledInfo: info, modal: truckModal };
     }
   };
 
-
-  const { entity, deteledInfo } = samplingDataEntity();
+  const { entity, deteledInfo, modal } = samplingDataEntity();
 
   return (
     <div className="admin_wrapper">
       <AdminPanelNav dataType={dataType} changeDataType={changeDataType} />
       <div className="admin_content">
         <AdminPanelSearchBar />
-        <AdminFilter dataType={dataType} handleChange={handleChangeFilterIndex} value={filterIndex}/>
+        <AdminFilter
+          dataType={dataType}
+          handleChange={handleChangeFilterIndex}
+          value={filterIndex}
+          handleOpenModal={handleOpen}
+        />
         <div className="flex admin_content-wrapper">
           <AdminList
             onClickHandle={setMapVissible}
@@ -93,6 +109,9 @@ const AdminPage: React.FC = memo(() => {
           {load ? deteledInfo : <div>waiting</div>}
         </div>
       </div>
+      <BasicModal open={open} handleClose={handleClose}>
+        <div>{!modal ? <></> : modal}</div>
+      </BasicModal>
     </div>
   );
 });
