65d8b70d61018c4807e92e2c0bf016a8578b0c0f
maksimbalantsevich <easypeasylemonskvii@gmail.com>
2023-03-25 13:36:59 +0300

    add: create truck http service

--- client/src/services/trucks.service.ts
+++ client/src/services/trucks.service.ts
@@ -3,6 +3,11 @@ import { $authHost } from './intex';
 
 
 const trucksService = {
+  createTruck: async(payload: any): Promise<TruckType> => {
+    const { data } = await $authHost.post('/trucks', payload);
+    return data;
+  },
+
   loadTrucks: async (): Promise<TruckType[]> => {
     const { data } = await $authHost.get('/trucks')
     return data;
