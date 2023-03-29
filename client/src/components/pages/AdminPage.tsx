1a6afd2bedd09282c8c96dd4809a9009ed9a4917
maksimbalantsevich <easypeasylemonskvii@gmail.com>
2023-03-29 13:24:08 +0300

    add: search & queryParams logic

--- client/src/components/pages/AdminPage.tsx
+++ client/src/components/pages/AdminPage.tsx
@@ -1,27 +1,30 @@
-import React, { memo, useState } from "react";
+import React, { memo, useEffect, useState } from "react";
 import AdminList from "../common/AdminList/AdminList";
 import AdminPanelNav from "../common/AdminPanelNav/AdminPanelNav";
 import AdminPanelSearchBar from "../common/AdminPanelSearchBar/AdminPanelSearchBar";
 import { useSelector } from "react-redux";
-import { getAllOrders } from "../../store/orders";
-import Map from "../common/DeteledItemInfo/Map/Map";
+import { getAllOrders, loadOrders } from "../../store/orders";
 import { DriverType, OrderType, TruckType } from "../../types/types";
 import { clearDriverErrors, getAllDrivers } from "../../store/drivers";
 import { clearTrucksErrors, getAllTrucks } from "../../store/trucks";
-import DriverDeteledInfo from "../common/DeteledItemInfo/DriverDeteledInfo/DriverDeteledInfo";
-import TruckDeteledInfo from "../common/DeteledItemInfo/TruckdeteledInfo/TruckDeteledInfo";
 import AdminFilter from "../common/AdminFilter/AdminFilter";
 import BasicModal from "../ui/Modal/Modal";
 import AddDriverForm from "../ui/AddDriverForm/AddDriverForm";
 import AddTuckForm from "../ui/AddTruckForm/AddTruckForm";
 import { useAppDispatch } from "../../store";
 import DeteledInfo from "../common/DeteledItemInfo/DeteledInfo";
+import { useSearchParams } from "react-router-dom";
 
 const AdminPage: React.FC = memo(() => {
-  const [load, setLoad] = useState(false);
+  const [load, setLoad] = useState(true);
   const [data, setData] = useState({} as OrderType | DriverType | TruckType);
   const [filterIndex, setFilterIndex] = useState(0);
   const [open, setOpen] = React.useState(false);
+  const [queryParams, setQueryParams] = useSearchParams('orders')
+  const id = queryParams.get('id');
+  useEffect(() => {
+    if (!id) setLoad(false)
+  }, [id])
 
   const dispatch = useAppDispatch();
 
@@ -33,7 +36,6 @@ const AdminPage: React.FC = memo(() => {
   const handleClose = () => {
     setOpen(false);
   };
-  const [dataType, setDataType] = useState("orders");
   const orders = useSelector(getAllOrders());
   const drivers = useSelector(getAllDrivers());
   const trucks = useSelector(getAllTrucks());
@@ -45,14 +47,15 @@ const AdminPage: React.FC = memo(() => {
     setFilterIndex(newValue);
   };
 
-  const setMapVissible = (data: OrderType) => {
+  const setDeteledInfoVissible = (data: OrderType) => {
+    setQueryParams({filter: String(queryParams.get('filter')), id: String(data.id)});
     setLoad(true);
     setData(data);
   };
   const changeDataType = (dataType: string) => {
-    setDataType(dataType);
     setLoad(false);
     setFilterIndex(0);
+    setQueryParams({filter: dataType});
   };
 
   const genOrdersEntityByFilter = () => {
@@ -91,10 +94,10 @@ const AdminPage: React.FC = memo(() => {
   };
 
   const samplingDataEntity = () => {
-    if (dataType === "orders") {
+    if (queryParams.get('filter') === "orders") {
       const filteredOrders = genOrdersEntityByFilter();
       return { entity: filteredOrders, modal: null };
-    } else if (dataType === "drivers") {
+    } else if (queryParams.get('filter') === "drivers") {
       const filteredDrivers = genDriversEntityByFilter();
       const driverModal = <AddDriverForm handleClose={handleClose} />;
       return { entity: filteredDrivers, modal: driverModal };
@@ -105,34 +108,48 @@ const AdminPage: React.FC = memo(() => {
     }
   };
 
+  const searchHandler = (event:React.SyntheticEvent, value:any) => {
+    if(orders.includes(value)) {
+      const filter = value.status === "not_assigned" ? 0 : value.status === "in_progress" ? 1 : 2
+      setQueryParams({filter: 'orders', id: value.id})
+      setFilterIndex(filter)
+    } else if (drivers.includes(value)) {
+      const filter = !value.truckId ? 0 : 1
+      setQueryParams({filter: 'drivers', id: value.id})
+      setFilterIndex(filter)
+    } else {
+      const filter = !value.driverId ? 0 : 1
+      setQueryParams({filter: 'trucks', id: value.id})
+      setFilterIndex(filter)
+    }
+  }
+
   const { entity, modal } = samplingDataEntity();
 
   return (
     <div className="admin_wrapper">
-      <AdminPanelNav dataType={dataType} changeDataType={changeDataType} />
+      <AdminPanelNav queryParams={queryParams} changeDataType={changeDataType} />
       <div className="admin_content">
-        <AdminPanelSearchBar />
+        <AdminPanelSearchBar searchHandler={searchHandler} />
         <AdminFilter
-          dataType={dataType}
+          dataType={queryParams}
           handleChange={handleChangeFilterIndex}
           value={filterIndex}
           handleOpenModal={handleOpen}
         />
-        {/* <div className="br-rght"> */}
-          <div className="flex admin_content-wrapper">
-            <AdminList
-              onClickHandle={setMapVissible}
-              dataType={dataType}
-              data={entity}
-            />
-            {load ? (
-              <DeteledInfo data={data} dataType={dataType} />
-            ) : (
-              <div>waiting</div>
-            )}
-          </div>
+        <div className="flex admin_content-wrapper">
+          <AdminList
+            onClickHandle={setDeteledInfoVissible}
+            dataType={queryParams}
+            data={entity}
+          />
+          {load || queryParams.get('id') ? (
+            <DeteledInfo data={data} dataType={queryParams} />
+          ) : (
+            <div>waiting</div>
+          )}
         </div>
-      {/* </div> */}
+      </div>
       <BasicModal open={open} handleClose={handleClose}>
         <div>{!modal ? <></> : modal}</div>
       </BasicModal>
@@ -141,3 +158,4 @@ const AdminPage: React.FC = memo(() => {
 });
 
 export default AdminPage;
+
