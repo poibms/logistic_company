5e36d095439a6ccb7e3a60eae9f108db2fbdabbc
maksimbalantsevich <easypeasylemonskvii@gmail.com>
2023-03-29 13:25:37 +0300

    update: getting truck info by search param

--- client/src/components/common/DeteledItemInfo/TruckdeteledInfo/TruckDeteledInfo.tsx
+++ client/src/components/common/DeteledItemInfo/TruckdeteledInfo/TruckDeteledInfo.tsx
@@ -1,15 +1,19 @@
 import * as React from "react";
-import { TruckType } from "../../../../types/types";
+import { useSelector } from "react-redux";
+import { useSearchParams } from "react-router-dom";
+import { getTruckById } from "../../../../store/trucks";
 
-type TruckInfoPropsType = {
-  truck: TruckType;
-};
 
-const TruckDeteledInfo: React.FC<TruckInfoPropsType> = ({ truck }) => {
+const TruckDeteledInfo: React.FC = () => {
+  const [searchParams] = useSearchParams();
+  const truckId = searchParams.get("id");
+  const truck = useSelector(getTruckById(+truckId!));
+
   return (
     <div className="deteledInfo">
       <div className="deteledInfo_wrapper flex flex_column">
         <div className="driverInfo">
+          {truck ? 
           <div className="flex align_center">
             <img
               src={`http://localhost:3007/${truck.photo}`}
@@ -23,10 +27,11 @@ const TruckDeteledInfo: React.FC<TruckInfoPropsType> = ({ truck }) => {
                 <b>Year of issue</b>: {truck.year} years old
               </p>
               <p>
-                <b>Load capacity</b>: {truck.loadCapacity} years old
+                <b>Load capacity</b>: {truck.loadCapacity} tons
               </p>
             </div>
           </div>
+          : <h1>There is no truck with such Id</h1>}
         </div>
       </div>
     </div>
