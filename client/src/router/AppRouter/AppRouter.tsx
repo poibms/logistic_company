bf072472f6345b44305f24497803c973501b7b1d
maksimbalantsevich <easypeasylemonskvii@gmail.com>
2023-04-04 14:50:47 +0300

    update: routes

--- client/src/router/AppRouter/AppRouter.tsx
+++ client/src/router/AppRouter/AppRouter.tsx
@@ -1,32 +1,50 @@
-import React, { Suspense } from 'react';
-import { privateRoutes, publicRoutes } from '../routes';
-import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { getIsLoggedIn } from '../../store/user';
-
-
+import React, { Suspense } from "react";
+import { adminRoutes, privateRoutes, publicRoutes } from "../routes";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { getIsLoggedIn, isAdmin as getIsAdmin } from "../../store/user";
+import Page404 from "../../components/pages/404Page/Page404";
 
 const AppRouter: React.FC = () => {
   const isLoggedIn = useSelector(getIsLoggedIn());
+  const isAdmin = useSelector(getIsAdmin());
   return (
-   <Suspense fallback={<></>}>
-    <Router>
-      <Routes>
-        { isLoggedIn ?
-          privateRoutes.map(route => 
+    <Suspense fallback={<></>}>
+      <Router>
+        <Routes>
+          {isLoggedIn &&
+            privateRoutes.map((route) =>
+              route.path ? (
+                <Route
+                  path={route.path}
+                  element={<route.component />}
+                  key={route.path}
+                />
+              ) : null
+            )}
+          {isAdmin &&
+            adminRoutes.map((route) =>
+              route.path ? (
+                <Route
+                  path={route.path}
+                  element={<route.component />}
+                  key={route.path}
+                />
+              ) : null
+            )}
+          {publicRoutes.map((route) =>
             route.path ? (
-              <Route path={route.path} element={<route.component/>} key={route.path} />
-            ) : null)
-        :
-          publicRoutes.map(route =>
-            route.path ? (
-              <Route path={route.path} element={<route.component/>} key={route.path} />
+              <Route
+                path={route.path}
+                element={<route.component />}
+                key={route.path}
+              />
             ) : null
-          )
-        }
-      </Routes>
-    </Router>
-   </Suspense>
+          )}
+          <Route path='*' element={<Page404/>} />
+        </Routes>
+      </Router>
+    </Suspense>
   );
 };
 
--- client/src/router/routes.ts
+++ client/src/router/routes.ts
@@ -1,13 +1,17 @@
 import AdminLayout from '../layouts/Admin';
 import MainLayout from '../layouts/Main';
+import ProfileLayout from '../layouts/ProfileLayout';
 import Sign from '../layouts/Sign';
 
 export const publicRoutes = [
   { path: '/', component: MainLayout},
   { path: '/login/:type?', component: Sign, exact: true },
+  {path: '/', component: MainLayout},
 ];
 
 export const privateRoutes = [
-  {path: '/', component: MainLayout},
-  {path: '/adminpanel', component: AdminLayout}
+  {path: '/profile', component: ProfileLayout},
+]
+export const adminRoutes = [
+  {path: '/adminpanel/*', component: AdminLayout}
 ]
