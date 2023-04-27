a6fcb03bb0d0b2d813256d606f3ea9bcd9be1ae2
maksimbalantsevich <easypeasylemonskvii@gmail.com>
2023-04-27 12:50:31 +0300

    update: news dynamic data

--- client/src/components/common/NewsList/NewsList.tsx
+++ client/src/components/common/NewsList/NewsList.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import News from '../News/News';
+import newsData from '../../../assets/imageSlider/News.json'
 
 const NewsList = () => {
   return (
     <section>
       <div className='news_index'>
         <div className='news_flex'>
+          {newsData.data.map((item) => (
+            <News image={item.image} text={item.text} date={item.date}/>
+          ))}
+          {/* <News />
           <News />
-          <News />
-          <News />
+          <News /> */}
         </div>
       </div>
     </section>
