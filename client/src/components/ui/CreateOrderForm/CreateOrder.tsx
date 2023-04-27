700e586d03e209cdc07a569610722c7036f74d16
maksimbalantsevich <easypeasylemonskvii@gmail.com>
2023-04-27 12:53:31 +0300

    update: select from and to point of order

--- client/src/components/ui/CreateOrderForm/CreateOrder.tsx
+++ client/src/components/ui/CreateOrderForm/CreateOrder.tsx
@@ -1,11 +1,21 @@
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from "@mui/material";
 import { YMaps, withYMaps } from "@pbe/react-yandex-maps";
+import axios from "axios";
 import * as React from "react";
 import { useSelector } from "react-redux";
 import { Form, useForm } from "../../../hooks/useForm";
 import { useAppDispatch } from "../../../store";
 import { createOrder, getOrdersErrors } from "../../../store/orders";
 import { OrderCreds } from "../../../types/types";
+import calculateDistance from "../../../utils/DistanceCalculator";
 import InputField from "../../common/InputField/InputField";
+import SelectInput from "../SelectInput/SelectInput";
 import validatorConfig from "./ValidatorConfig";
 
 const initialData: OrderCreds = {
@@ -21,9 +31,26 @@ type CreateOrderPropsType = {};
 const CreateOrder: React.FC<CreateOrderPropsType> = () => {
   const [selectedFile, setSelectedFile] = React.useState(null);
   const [fileError, setFileError] = React.useState(false);
+  const [distance, setDistance] = React.useState(0);
+  const [distanceError, setDistanceError] = React.useState("");
+  const [cities, setCities] = React.useState([{}]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [from, setFrom] = React.useState("");
+  const [to, setTo] = React.useState("");
 
   const dispatch = useAppDispatch();
   const orderErrors = useSelector(getOrdersErrors());
+
+  React.useEffect(() => {
+    axios
+      .get("https://mainapi.nrg-tk.ru/v3/cities?lang=ru", {
+        headers: {
+          "NrgApi-DevToken":
+            "MMGPa7NQ2HwhiHCwDnuQqIWZFJnPYrbX8vBap8StkkrMChvLHiv7OEttx5QFy2kK",
+        },
+      })
+      .then((data) => citiesHandler(data.data.cityList));
+  }, []);
 
   const {
     data,
@@ -39,6 +66,15 @@ const CreateOrder: React.FC<CreateOrderPropsType> = () => {
     setFileError(false);
   };
 
+  const citiesHandler = (cityList: any) => {
+    const filteredArray = cityList.filter((obj: any) =>
+      obj.name.includes("BY")
+    );
+    // const citiesName = cityList.filter((item: any) => item.parentId === -1);
+    setCities(filteredArray);
+    setIsLoading(false);
+  };
+
   const handleSubmit = async (e: React.FormEvent<HTMLButtonElement>) => {
     e.preventDefault();
     if (validate(data)) {
@@ -52,33 +88,100 @@ const CreateOrder: React.FC<CreateOrderPropsType> = () => {
         dispatch(createOrder(formData));
         handleResetForm(e);
         setSelectedFile(null);
+        setDistance(0);
       } else {
         setFileError(true);
       }
     }
   };
 
+  const handleCalculateDistance = async (
+    e: React.FormEvent<HTMLButtonElement>
+  ) => {
+    e.preventDefault();
+    setDistanceError("");
+    const calc = await calculateDistance(from, to);
+    if (calc === 0) {
+      setDistanceError(
+        "Something went wrong while calculating the distance, check the correctness of the filled fields"
+      );
+    } else {
+      setDistance(Math.round(calc));
+    }
+  };
+
+  const handleChange = (event: SelectChangeEvent) => {
+    console.log(event.target.value);
+    setFrom(event.target.value);
+  };
+
+  const handleChangeTo = (event: SelectChangeEvent) => {
+    console.log(event.target.value);
+    setTo(event.target.value);
+  };
+
+
   return (
-    <div>
-      <h2>Create new Order</h2>
-      <Form data={data} errors={errors} handleChange={handleInputChange}>
-        <InputField name="name" label="Name" autoFocus />
-        <InputField name="weight" label="Weight" />
-        <InputField name="from" label="From" />
-        <InputField name="to" label="To" />
-        <input className="mt-10" type="file" onInput={handleFileSelect} />
-        <button
-          className="button_outline button_modal"
-          type="submit"
-          onClick={handleSubmit}
-          disabled={enterError ? true : false}
-        >
-          Create order
-        </button>
-      </Form>
-      {orderErrors && <p className="form_error">{orderErrors}</p>}
-      {fileError && <p className="form_error">File is required</p>}
-    </div>
+    <>
+      {isLoading ? (
+        <>Loading</>
+      ) : (
+        <div>
+          <h2>Create new Order</h2>
+          <Form data={data} errors={errors} handleChange={handleInputChange}>
+            <InputField name="name" label="Name" autoFocus />
+            <InputField name="weight" label="Weight" />
+            <div className="form_select_inner flex">
+              <SelectInput
+                label="From"
+                name="from"
+                value={from}
+                items={cities}
+                handleChange={handleChange}
+              />
+              <SelectInput
+                label="to"
+                name='to'
+                value={to}
+                items={cities}
+                handleChange={handleChangeTo}
+              />
+            </div>
+            <input className="mt-10" type="file" onInput={handleFileSelect} />
+            <div className="flex calculate_inner justify-between">
+              {distance !== 0 ? (
+                <h4>Distance between cities: {distance} km</h4>
+              ) : (
+                <h4>
+                  Fill the From & To fields and press the callculate button
+                </h4>
+              )}
+
+              <button
+                className="button_outline "
+                type="submit"
+                onClick={handleCalculateDistance}
+                disabled={enterError ? true : false}
+              >
+                calculate
+              </button>
+            </div>
+
+            <button
+              className="button_outline button_modal"
+              type="submit"
+              onClick={handleSubmit}
+              disabled={enterError ? true : false}
+            >
+              Create order
+            </button>
+          </Form>
+          {orderErrors && <p className="form_error">{orderErrors}</p>}
+          {distanceError && <p className="form_error">{distanceError}</p>}
+          {fileError && <p className="form_error">File is required</p>}
+        </div>
+      )}
+    </>
   );
 };
 
