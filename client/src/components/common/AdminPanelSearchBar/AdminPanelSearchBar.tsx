f596cb762230181c2139fee2c2cec766c0ba7759
maksimbalantsevich <easypeasylemonskvii@gmail.com>
2023-05-10 13:16:43 +0300

    update: search admin ui

--- client/src/components/common/AdminPanelSearchBar/AdminPanelSearchBar.tsx
+++ client/src/components/common/AdminPanelSearchBar/AdminPanelSearchBar.tsx
@@ -1,25 +1,34 @@
 import * as React from "react";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import SearchInput from "../../ui/SearchInput/SearchInput";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import Logo from "../../../assets/Logomark.png";
 
 type SearchBarType = {
   searchHandler: any;
 };
 
 const AdminPanelSearchBar: React.FC<SearchBarType> = ({ searchHandler }) => {
+  const navigate = useNavigate();
   return (
     <div className="searchbar">
       <div className="searchbar_wrapper">
-        <div className="searchbar_title">
-          <h3>DASHBOARD</h3>
+        <div className="searchbar_flex">
+          <div className="searchbar_img" onClick={() => navigate("/")}>
+            <img src={Logo} alt="logo" />
+          </div>
+          <div className="searchbar_title">
+            <h3>DASHBOARD</h3>
+          </div>
         </div>
-        <div className="searchbar_input">
-          <SearchInput searchHandler={searchHandler} />
-        </div>
+        <div className="searchbar_flex">
+          <div className="searchbar_input">
+            <SearchInput searchHandler={searchHandler} />
+          </div>
           <NavLink to="/adminpanel">
             <AccountCircleIcon />
           </NavLink>
+        </div>
       </div>
     </div>
   );
